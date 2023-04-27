130b1c341fbdac537c5d699c156c6a2d91101588
44jovi <kwong.jovi@gmail.com>
2023-04-27 16:30:18 +0100

    component ProductTable

--- react-project/src/components/ProductTable.jsx
+++ react-project/src/components/ProductTable.jsx
@@ -1,22 +1,20 @@
 import { useState } from "react";
 
 const ProductTable = () => {
+  const products = ["apple", "banana", "cheese"];
   const [searchTerm, setSearchTerm] = useState(null);
-  const [results, setResults] = useState("results will show here!");
 
   return (
     <>
       <h2>product table time!</h2>
-      <h3>results based on your search term...</h3>
-      <p>{results}</p>
-      <form>
-        <input
-          placeholder="enter search term"
-          onChange={(e) => {
-            setSearchTerm(e.target.value);
-          }}
-        />
-      </form>
+      <h3>type "apple" or "juice"</h3>
+      <p>{products.filter((p) => p.toLowerCase().startsWith(searchTerm))}</p>
+      <input
+        name="searchTerm"
+        placeholder="type here!"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
     </>
   );
 };
