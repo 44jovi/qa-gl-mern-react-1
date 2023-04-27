894a8cdd30e12c9018dcca1ff8b220dd908e8b84
44jovi <kwong.jovi@gmail.com>
2023-04-27 10:29:49 +0100

    initial setup and Cat component

---
+++ react-project/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Cat from './components/Cat'
+
+function App() {
+  return (
+    // fragments <> instead of <div>
+    <>
+      <Cat/>
+    </>
+  );
+}
+
+export default App;
---
+++ react-project/src/components/Cat.jsx
@@ -0,0 +1,9 @@
+const Cat = () => {
+    return (
+    <div>
+        <h1>Howdy, y'all</h1>
+        <p>How you doin'?</p>
+    </div>
+)};
+
+export default Cat;
