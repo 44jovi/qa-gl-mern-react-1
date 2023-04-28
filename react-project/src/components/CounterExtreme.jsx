c8497899731592c39ce35e45a5b30f0871e12770
44jovi <kwong.jovi@gmail.com>
2023-04-28 11:59:46 +0100

    reset function

--- react-project/src/components/CounterExtreme.jsx
+++ react-project/src/components/CounterExtreme.jsx
@@ -2,20 +2,21 @@ import { useState } from "react";
 
 const CounterExtreme = () => {
   const [count, setCount] = useState(0);
-  const [prevCount, setPrevCount] = useState(0);
-  const [history, setHistory] = useState([0]);
-
-  // const history = [];
+  const [history, setHistory] = useState([]);
 
   const btnPress = (e) => {
     e.preventDefault();
-
     let numChange = parseInt(e.target.innerHTML);
     setCount(count + numChange);
     setHistory([count, ...history]);
     console.log(history);
   };
 
+  const reset = (e) => {
+    e.preventDefault();
+    setCount(0);
+    setHistory([]);
+  };
   return (
     <>
       {/* current count display */}
@@ -23,10 +24,9 @@ const CounterExtreme = () => {
       {/* counter buttons */}
       <button onClick={btnPress}>+1</button>
       <button onClick={btnPress}>+5</button>
-      <button onClick={() => setCount(0)}>RESET</button>
+      <button onClick={reset}>RESET</button>
       <button onClick={btnPress}>-1</button>
       <button onClick={btnPress}>-5</button>
-      <div>test - previous count: {prevCount}</div>
       <p>{history.map((i) => i)}</p>
     </>
   );
