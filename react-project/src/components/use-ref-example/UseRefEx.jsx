0dcba686055c8301e247e08a3a2d91063e46bbd8
44jovi <kwong.jovi@gmail.com>
2023-05-03 16:55:34 +0100

    update UseRefEx

--- react-project/src/components/use-ref-example/UseRefEx.jsx
+++ react-project/src/components/use-ref-example/UseRefEx.jsx
@@ -1,43 +1,56 @@
 import { useRef, useState } from "react";
+
 function UseRefEx() {
+  // Multiple renders of values stored by useRef will always only render the values stored during the first render.
+  // Even if the values stored change.
   const arrayOfResults = useRef([0, 0]);
 
   const [result, setResult] = useState([]);
 
   function coinFlip(e) {
     e.preventDefault();
-    const result = Math.random();
+    console.log("FLIP BUTTON PRESSED");
 
-    if (result <= 0.5) {
+    const randNum = Math.random();
+    console.log("random number: " + randNum);
+
+    if (randNum <= 0.5) {
       arrayOfResults.current[0] += 1;
       console.log("HEADS!");
     } else {
       arrayOfResults.current[1] += 1;
       console.log("TAILS!");
     }
-    console.log("RESULT");
-    console.log(result);
-    console.log(arrayOfResults.current);
+    console.log("useRef: " + arrayOfResults.current);
   }
 
-  function resultBtn() {
+  function handleResultBtn() {
     setResult(arrayOfResults.current);
     console.log("RESULT BUTTON PRESSED");
-    console.log(arrayOfResults.current);
-    console.log(result);
+    console.log("useState: " + result);
+    console.log("useRef: " + arrayOfResults.current);
   }
 
   return (
     <>
-      <h1>Coin Flip Game!</h1>
-      <button onClick={coinFlip}>FLIP</button>
-      <button onClick={resultBtn}>RESULTS</button>
+      <h1>COIN FLIP GAME</h1>
+      <button onClick={coinFlip} style={{ backgroundColor: "pink" }}>
+        FLIP
+      </button>
+      <button
+        onClick={handleResultBtn}
+        style={{ backgroundColor: "lightgreen" }}
+      >
+        RESULTS
+      </button>
       <br />
       <p>HEADS: {result[0]}</p>
       <p>TAILS: {result[1]}</p>
+      <p>Want to see the results more than once?</p>
       <p>
-        To see the results more than once, subscribe to our premium service!
+        Well, you can't because we are using a useRef hook to persist the data.
       </p>
+      <p>Goodbye.</p>
     </>
   );
 }
